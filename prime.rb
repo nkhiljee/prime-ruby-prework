e409a37b6e840f4abdd3411c86a7ec384d534c72
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 06:02:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,9 @@
 def prime?(num)
-  (2..(num - 1)).each do |n|
-    return false if num % n == 0
-  end
-  true
-end
-
-
-def prime?(num)
   false if num < 2
   (2 .. num).each do |factor|
     if (num % factor).zero?
       return false
     end
+  end
+  true
 end
