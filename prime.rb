65bcfe7ae061500c4acb1f00939d2677f96e2267
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 06:07:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,4 @@
- def primetest
- for test_value in [2, 3, 5, 7, 9, 11, 13] do
-  if (@nth_value % test_value) == 0
-   puts ("#{@nth_value} is not divisible by #{test_value}")
-  else
-   puts ("This is not a prime number since this is divisible by #{test_value}")
-  break
-  end
- end
- end
+def is_prime?(num)
+  return if num <= 1
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 end
